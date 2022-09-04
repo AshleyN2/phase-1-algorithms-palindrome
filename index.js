6e2c9b69ba2eac4c239a047a7c610b5ba21e15f9
Ashley <ashleynjeri7@gmail.com>
2022-09-04 12:32:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const arrayValues = word.split(''); // converting our string to an array
+  const reverseArrayValues = arrayValues.reverse() // reversing our array values
+  // checks whether characters match
+  const reverseWord = reverseArrayValues.join(''); // converting our array back to string
+  if (word.toLowerCase() === reverseWord) { //checking our word to lowercase
+    console.log('Is a palindrome')
+    return true
+  } else {
+    console.log('not a palindrome')
+    return false
+  }
 }
+isPalindrome('abba');
 
 /* 
   Add your pseudocode here
+  
+declare a function isPalindrome(that receives one string argument)
+iterate over the string argument
+  if the string(in lowercase) is a palidrome 
+    return true
+  else
+    return false
 */
+
 
 /*
   Add written explanation of your solution here
+
+I converted the string to an array and then reversed the characters in the array 
+to check if they were matching hence a palindrome.
+
+I added console.log() to test the output
+
 */
 
 // You can run `node index.js` to view these console logs
